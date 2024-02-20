1861cd54a6472b327b8d7e0ec1a8464e5f3b9af1
Denis Volk <denis.volk@gmail.com>
2024-02-20 18:34:15 +0000

    remove unused imports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException, Query
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
